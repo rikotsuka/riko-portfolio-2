fba6100a07840f55046c38b3ac917420e266e8c3
Rikotsuka <meronpanhanzoumon@gmail.com>
2023-08-18 18:21:44 +0900

    遷移先作成

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-//テキストのカウントアップ+バーの設定
+//テキストのカウントアップの設定
 var bar = new ProgressBar.Line(splash_text, {//id名を指定
-	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+	strokeWidth: 0,//進捗ゲージの太さ
 	duration: 1000,//時間指定(1000＝1秒)
-	strokeWidth: 0.2,//進捗ゲージの太さ
-	color: '#555',//進捗ゲージのカラー
-	trailWidth: 0.2,//ゲージベースの線の太さ
-	trailColor: '#bbb',//ゲージベースの線のカラー
-	text: {//テキストの形状を直接指定				
+	trailWidth: 0,//線の太さ
+	text: {//テキストの形状を直接指定	
 		style: {//天地中央に配置
-			position: 'absolute',
-			left: '50%',
-			top: '50%',
-			padding: '0',
-			margin: '-30px 0 0 0',//バーより上に配置
+			position:'absolute',
+			left:'50%',
+			top:'50%',
+			padding:'0',
+			margin:'0',
 			transform:'translate(-50%,-50%)',
-			'font-size':'1rem',
-			color: '#fff',
+			'font-size':'1.2rem',
+			color:'#fff',
 		},
 		autoStyleContainer: false //自動付与のスタイルを切る
 	},
